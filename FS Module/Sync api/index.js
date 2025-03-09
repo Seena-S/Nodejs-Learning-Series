0ad5363c4a3e2a6c64549536f089e9ac462943cd
Seena George <seenageorge167@gmail.com>
2025-03-09 16:01:23 +0530

    Update index.js

--- FS Module/Sync api/index.js
+++ FS Module/Sync api/index.js
@@ -2,58 +2,35 @@
 import * as fs from "fs";
 
 // Creating Directory
-// fs.mkdir("c:\\nodejs\\filesystem", (error) => {
-//   if (error) throw error;
-//   console.log("Directory Created...");
-// });
+// fs.mkdirSync("c:\\nodejs\\filesystem");
 
 // Creating Directory - Path not required
-fs.mkdir("c:\\nodejs\\test\\demo", { recursive: true }, (error) => {
-  if (error) throw error;
-  console.log("Directory Created...");
-});
+// fs.mkdirSync("c:\\nodejs\\test\\demo", { recursive: true });
 
 // Read content of directory
-// fs.readdir("c:\\nodejs\\", (error, files) => {
-//   if (error) throw error;
-//   for (const file of files) {
-//     console.log(file);
-//   }
-// });
+// const files = fs.readdirSync("c:\\nodejs\\");
+// for (const file of files) {
+//   console.log(file);
+// }
 
 // Remove Directory - Directory should be empty.
-// fs.rmdir("c:\\nodejs\\fs", (error) => {
-//   if (error) throw error;
-//   console.log("Directory Removed");
-// });
+// fs.rmdirSync("c:\\nodejs\\fs");
 
 // Create & Write File
-// fs.writeFile("readme.txt", "Hello Nodejs", (error) => {
-//   if (error) throw error;
-//   console.log("File Created ...");
-// });
+// fs.writeFileSync("readme.txt", "Hello Nodejs");
 
 // Read File
-// fs.readFile("readme.txt", "utf-8", (error, data) => {
-//   if (error) throw error;
-//   console.log(data);
-// });
+// const result = fs.readFileSync("readme.txt", "utf-8");
+// console.log(result);
 
 // Append data into file
-// fs.appendFile("readme.txt", "HuXn WebDev", (error) => {
-//   if (error) throw error;
-//   console.log("Data Appended ...");
-// });
+// fs.appendFileSync("readme.txt", "HuXn WebDev");
 
 // Copy File
-// fs.copyFile("readme.txt", "info.txt", (error) => {
-//   if (error) throw error;
-//   console.log("File Copied ...");
-// });
+// fs.copyFileSync("readme.txt", "info.txt");
 
 // Get file information
-// fs.stat("info.txt", (error, stats) => {
-//   if (error) throw error;
-//   console.log(stats.isDirectory());
-//   console.log(stats.isFile());
-// });
+const stats = fs.statSync("info.txt");
+console.log(stats.isDirectory());
+console.log(stats.isFile());
+console.log(stats);
